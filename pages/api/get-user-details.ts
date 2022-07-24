4ab100db8647fdace66fb41694ce97feb4334d77
ryanlabsglobal <ryan.s@soslab.global>
2022-07-25 08:41:49 +1000

    New get user detail api

---
+++ pages/api/get-user-details.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { Client } from "pg";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const portNum = parseInt(process.env.PGPORT ? process.env.PGPORT : "");
+  const { body } = req;
+  const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: portNum,
+  });
+  client.connect();
+
+  const query = {
+    name: "fetch-user-details",
+    text: "SELECT * FROM users WHERE wallet_key = $1",
+    values: [body],
+  };
+  try {
+    const data = await client.query(query);
+
+    const response = data.rows[0];
+
+    res.status(200).json({ res: response });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  } finally {
+    client.end();
+  }
+};
+
+export default handler;
